7a3f7fc69d4e3712bb73c39ab59931c480b9b633
Nghi Ngo <tuongnghi.ngonguyen@gmail.com>
2024-09-13 10:42:15 +0700

    feat: in progress

--- app.py
+++ app.py
@@ -111,8 +111,8 @@ def upload_file():
     else:
         return jsonify({"error": "Invalid file type"}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 # if __name__ == '__main__':
 #     app.run(host='0.0.0.0', port=5000)
